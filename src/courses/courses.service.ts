2e12eb3f65c72a162a9277fb9df2e3ba13703aaa
AtanasRaykovPapucharov <atanas.papucharov@gmail.com>
2023-09-14 12:28:36 +0300

    modules and controllers

---
+++ src/courses/courses.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCourseDto } from './dto/create-course.dto';
+import { UpdateCourseDto } from './dto/update-course.dto';
+
+@Injectable()
+export class CoursesService {
+  create(createCourseDto: CreateCourseDto) {
+    return `This action adds a new course with name: ${createCourseDto.name}`;
+  }
+
+  findAll(type?: string) {
+    return `This action returns all courses${ type ? " by type-" +  type : "" }`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} course`;
+  }
+
+  update(id: number, updateCourseDto: UpdateCourseDto) {
+    return `This action updates a #${id} course`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} course`;
+  }
+}
